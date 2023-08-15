fb04c88a08fd23a590252600e2cec2d7344405aa
Denys Cherednychenko <cherydens@ukr.net>
2023-08-15 11:44:10 +0300

    New

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  flex-direction: column;
+
+  width: 320px;
+  padding-top: 24px;
+  padding-bottom: 24px;
+`;
---
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  gap: 16px;
+  padding: 8px;
+  border: 1px solid gray;
+  margin-bottom: 16px;
+`;
+
+export const FormLabel = styled.label`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  gap: 4px;
+`;
